8211ef80c1e7bd16b652b34cf91a3b22be86909e
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2020-07-24 19:47:57 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function carregar() {
+    // var rodape = document.querySelector('footer#rodape')
+    // var cabec = document.querySelector('header#cabec') 
+    var msg = document.querySelector('div#msg')
+    var data = new Date()
+    var data1 = new Date()
+    var mins = data1.getMinutes()
+    var hora = data.getHours()
+    msg.innerHTML = `Horário atual: ${hora}:${mins}`
+
+
+    if (hora >= 0 && hora < 12) {
+        document.body.style.backgroundImage = "url('dia.png')"
+    } else if (hora >= 12 && hora <= 18) {
+        document.body.style.backgroundImage = "url('tarde.png')"
+    } else {
+        document.body.style.backgroundImage = "url('noite.png')"
+    }
+}
